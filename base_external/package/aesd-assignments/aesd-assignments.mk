f06bf63b2b6cf788a949e926230fed6d98d12450
dileep92s <dileep92s@gmail.com>
2026-01-07 16:38:32 +0000

    fix perror

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 031e073c30a2fb1a55dabb7023d1d48bf5609000
+AESD_ASSIGNMENTS_VERSION = a77bc83fea8f8cba920677d5339121400f49c27c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
